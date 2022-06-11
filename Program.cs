a3f8795761164bb8bf11789906993b640a93972b
Eduardo Lima <eduardlimmadev@gmail.com>
2022-06-11 15:11:20 -0300

    refactor(Program): refactoring database access using Dapper

--- Program.cs
+++ Program.cs
@@ -1,22 +1,14 @@
-﻿using Microsoft.Data.SqlClient;
+﻿using BaltaDataAccess.Models;
+using Dapper;
+using Microsoft.Data.SqlClient;
 
 const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$;Encrypt=False";
 
 using (var connection = new SqlConnection(connectionString))
 {
-  Console.WriteLine("Conectado");
-  connection.Open();
-
-  using (var command = new SqlCommand()) {
-    command.Connection = connection;
-    command.CommandType = System.Data.CommandType.Text;
-    command.CommandText = "SELECT [Id], [Title] FROM [Category]";
-
-    var reader = command.ExecuteReader();
-    while (reader.Read()) {
-      Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
-    }
+  var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+  foreach (var category in categories)
+  {
+    Console.WriteLine($"{category.Id} - {category.Title}");
   }
-
-  connection.Close();
 }
