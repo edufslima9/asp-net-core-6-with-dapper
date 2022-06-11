f91189a3671fb162e1bad7fa7a284874e7e1bf87
Eduardo Felipe <edufslima9@gmail.com>
2022-06-11 14:59:55 -0300

    feat(Program): Test database connection using ADO.NET (SQLServer)

--- Program.cs
+++ Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using Microsoft.Data.SqlClient;
+
+const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$;Encrypt=False";
+
+using (var connection = new SqlConnection(connectionString))
+{
+  Console.WriteLine("Conectado");
+  connection.Open();
+
+  using (var command = new SqlCommand()) {
+    command.Connection = connection;
+    command.CommandType = System.Data.CommandType.Text;
+    command.CommandText = "SELECT [Id], [Title] FROM [Category]";
+
+    var reader = command.ExecuteReader();
+    while (reader.Read()) {
+      Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
+    }
+  }
+
+  connection.Close();
+}
