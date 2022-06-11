c2f7e485647c56beab56a772e77a1cdaead9d638
Eduardo Lima <eduardlimmadev@gmail.com>
2022-06-11 15:25:19 -0300

    feat(Program): Insert data with Dapper

--- Program.cs
+++ Program.cs
@@ -4,11 +4,45 @@ using Microsoft.Data.SqlClient;
 
 const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$;Encrypt=False";
 
+var category = new Category {
+  Id = Guid.NewGuid(),
+  Title = "Amazon AWS",
+  Url = "amazon",
+  Summary = "AWS Cloud",
+  Order = 8,
+  Description = "Categoria destinada a serviços do AWS",
+  Featured = false
+};
+
+var insertSql = @"INSERT INTO
+      [Category]
+    VALUES (
+      @Id,
+      @Title,
+      @Url,
+      @Summary,
+      @Order,
+      @Description,
+      @Featured
+    )";
+
 using (var connection = new SqlConnection(connectionString))
 {
+  var rows = connection.Execute(insertSql, new {
+    category.Id,
+    category.Title,
+    category.Url,
+    category.Summary,
+    category.Order,
+    category.Description,
+    category.Featured
+  });
+
+  Console.WriteLine($"{rows} linhas inseridas");
+
   var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
-  foreach (var category in categories)
+  foreach (var item in categories)
   {
-    Console.WriteLine($"{category.Id} - {category.Title}");
+    Console.WriteLine($"{item.Id} - {item.Title}");
   }
 }
