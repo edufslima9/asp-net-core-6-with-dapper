d6fe9fed837c462e306bdf45a8ffd97b6219fd92
Eduardo Felipe <edufslima9@gmail.com>
2022-06-13 12:42:35 -0300

    feat(Program): adding GET functionality

--- Program.cs
+++ Program.cs
@@ -9,7 +9,8 @@ using (var connection = new SqlConnection(connectionString))
   //CreateCategory(connection);
   //UpdateCategory(connection);
   // DeleteCategory(connection);
-  ListCategories(connection);
+  //ListCategories(connection);
+  GetCategory(connection);
 }
 
 static void ListCategories(SqlConnection connection)
@@ -19,6 +20,17 @@ static void ListCategories(SqlConnection connection)
   {
     Console.WriteLine($"{item.Id} - {item.Title}");
   }
+}
+
+static void GetCategory(SqlConnection connection) {
+  var category = connection
+    .QueryFirstOrDefault<Category>(
+      "SELECT TOP 1 [Id], [Title] FROM [Category] WHERE [Id]=@id",
+      new
+      {
+        id = "af3407aa-11ae-4621-a2ef-2028b85507c4"
+      });
+  Console.WriteLine($"{category.Id} - {category.Title}");
 }
 
 static void CreateCategory(SqlConnection connection)
