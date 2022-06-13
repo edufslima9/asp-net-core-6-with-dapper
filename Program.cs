01026661934dad48492e8ab1a354c8713aeb363a
Eduardo Lima <eduardlimmadev@gmail.com>
2022-06-13 12:48:32 -0300

    feat(Program): adding ExecuteMany functionality

--- Program.cs
+++ Program.cs
@@ -7,10 +7,11 @@ const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa
 using (var connection = new SqlConnection(connectionString))
 {
   //CreateCategory(connection);
+  //CreateManyCategory(connection);
   //UpdateCategory(connection);
-  // DeleteCategory(connection);
-  //ListCategories(connection);
-  GetCategory(connection);
+  //DeleteCategory(connection);
+  ListCategories(connection);
+  //GetCategory(connection);
 }
 
 static void ListCategories(SqlConnection connection)
@@ -70,6 +71,63 @@ var insertSql = @"INSERT INTO
     Console.WriteLine($"{rows} linhas inseridas");
 }
 
+static void CreateManyCategory(SqlConnection connection)
+{
+  var category = new Category {
+    Id = Guid.NewGuid(),
+    Title = "Amazon AWS",
+    Url = "amazon",
+    Summary = "AWS Cloud",
+    Order = 8,
+    Description = "Categoria destinada a serviços do AWS",
+    Featured = false
+  };
+
+  var category2 = new Category {
+    Id = Guid.NewGuid(),
+    Title = "Categoria Nova",
+    Url = "categoria-nova",
+    Summary = "Categoria Nova",
+    Order = 9,
+    Description = "Categoria",
+    Featured = true
+  };
+
+var insertSql = @"INSERT INTO
+      [Category]
+    VALUES (
+      @Id,
+      @Title,
+      @Url,
+      @Summary,
+      @Order,
+      @Description,
+      @Featured
+    )";
+
+  var rows = connection.Execute(insertSql, new []{
+    new {
+      category.Id,
+      category.Title,
+      category.Url,
+      category.Summary,
+      category.Order,
+      category.Description,
+      category.Featured
+    },
+    new {
+      category2.Id,
+      category2.Title,
+      category2.Url,
+      category2.Summary,
+      category2.Order,
+      category2.Description,
+      category2.Featured
+    }});
+
+    Console.WriteLine($"{rows} linhas inseridas");
+}
+
 static void UpdateCategory(SqlConnection connection)
 {
   var updateQuery = @"UPDATE [Category]
@@ -91,7 +149,7 @@ static void DeleteCategory(SqlConnection connection)
   var deleteQuery = "DELETE FROM [Category] WHERE [Id]=@Id";
   
   var rows = connection.Execute(deleteQuery, new {
-    Id = new Guid("af3407aa-11ae-4621-a2ef-2028b85507c4")
+    Id = new Guid("00000000-0000-0000-0000-000000000000")
   });
 
   Console.WriteLine($"{rows} registros excluídos");
